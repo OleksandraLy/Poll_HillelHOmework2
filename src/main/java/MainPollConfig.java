8dc1291585deca8acaf2378e3f4c1dd9a9e214a3
OleksandraLy <ol.lystopad@gmail.com>
2022-01-14 21:42:22 +0200

    poll service commit 1 - config corrected, beans added

--- src/main/java/MainPollConfig.java
+++ src/main/java/MainPollConfig.java
@@ -1,12 +1,8 @@
 import entities.PollParticipant;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import services.AskQuestions;
-import java.io.IOException;
 
 public class MainPollConfig {
-    public static void main(String[] args) throws IOException {
-        AskQuestions askQuestionsObject = new AskQuestions();
-        AskQuestions.askQuestionsMethod();
+    public static void main(String[] args) {
         ClassPathXmlApplicationContext pollContext = new ClassPathXmlApplicationContext("Context.xml");
         PollParticipant participantBean = (PollParticipant) pollContext.getBean("pollParticipant");
         pollContext.close();
